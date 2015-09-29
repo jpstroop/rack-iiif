81c075c1185f032cb12c83b8d6ee62f14e10a953
Tom Johnson <tom@dp.la>
2015-09-29 12:22:46 -0700

    Add outline of middleware for routing and handling
    Separating routing from response handling, with the idea that domain
    objects will handle each.

--- lib/rack/iiif.rb
+++ lib/rack/iiif.rb
@@ -6,18 +6,41 @@ module Rack
   # 
   # @see http://www.rubydoc.info/github/rack/rack/master/file/SPEC for the Rack specification
   # @see http://iiif.io/api/image/2.1/ for the IIIF Image API 2.1 (draft)
-  class IIIF
+  module IIIF
     ##
-    # @param [#call] app
-    def initialize(app)
-      @app = app
+    # Handles responses from IIIF service classes
+    class Handler
+      ##
+      # @param [#call] app
+      def initialize(app)
+        @app = app
+      end
+      
+      ##
+      # @param [Hash] env
+      def call(env)
+        status, headers, response = @app.call(env)
+        [status, headers, response]
+      # rescue MyErrorType => e
+      #   [e.status, e.headers, [e.message]]
+      end
     end
-    
+
     ##
-    # @param [Hash] env
-    def call(env)
-      status, headers, response = @app.call(env)
-      [status, headers, response]
+    # Converts a request to IIIF response objects accrding to the request path
+    class Router
+      ##
+      # @param [#call] app
+      def initialize(app)
+        @app = app
+      end
+      
+      ##
+      # @param [Hash] env
+      # @return [Array<Integer, Hash, IIIF::Response>] the response
+      def call(env)
+        @app.call(env)
+      end
     end
   end
 end
--- spec/lib/rack/iiif_spec.rb
+++ spec/lib/rack/iiif_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper.rb'
 require 'rack/test'
 
-describe Rack::IIIF do
+describe 'middleware' do
   include ::Rack::Test::Methods
 
+  let(:headers) { { header: 'value' } }
   let(:base_app) do
-    double("Target Rack Application", :call => [200, {}, []])
+    double("Target Rack Application", :call => [200, headers, []])
   end
 
   let(:app) { described_class.new(base_app) }
-  
-  describe '#call' do
-    it 'returns a 200 status code' do
-      get '/'
-      expect(last_response.status).to eq 200
+
+  describe Rack::IIIF::Handler do
+    describe '#call' do
+      it 'returns a 200 status code' do
+        get '/'
+        expect(last_response.status).to eq 200
+      end
+      
+      it 'retains headers' do
+        get '/'
+        expect(last_response.headers).to eq headers
+      end
+    end
+  end
+
+  describe Rack::IIIF::Router do
+    describe '#call' do
+      it { expect { get('/') }.not_to raise_error }
     end
   end
 end
