6823a89d64d3e5d484b486392e6475a3a248338f
Tom Johnson <tom@dp.la>
2015-09-29 11:47:57 -0700

    Add the outline of IIIF Rack Middleware
    Implements an echo call and a get '/' test.

--- lib/rack/iiif.rb
+++ lib/rack/iiif.rb
@@ -2,5 +2,13 @@ require 'rack'
 
 module Rack
   class IIIF
+    def initialize(app)
+      @app = app
+    end
+    
+    def call(env)
+      status, headers, response = @app.call(env)
+      [status, headers, response]
+    end
   end
 end
--- spec/lib/rack/iiif_spec.rb
+++ spec/lib/rack/iiif_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper.rb'
+require 'rack/test'
 
 describe Rack::IIIF do
-  it { is_expected.to be_a Rack::IIIF }
+  include ::Rack::Test::Methods
+
+  let(:base_app) do
+    double("Target Rack Application", :call => [200, {}, []])
+  end
+
+  let(:app) { described_class.new(base_app) }
+  
+  describe '#call' do
+    it 'returns a 200 status code' do
+      get '/'
+      expect(last_response.status).to eq 200
+    end
+  end
 end
