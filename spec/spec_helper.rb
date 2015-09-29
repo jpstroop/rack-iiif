b84cf4ec30b2ce2a193b1d4193e39199674713c6
Tom Johnson <tom@dp.la>
2015-09-29 08:24:56 -0700

    Add color for RSpec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'rack/iiif'
 Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
+  config.color = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.exclusion_filter = {:ruby => lambda { |version|
