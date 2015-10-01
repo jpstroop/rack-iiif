867fd11ec029db763430067e2a3b3076e7b4896b
Tom Johnson <tom@dp.la>
2015-10-01 07:32:02 -0700

    Add Rotation parameter
    Implements Rotation with required validity constraints, access to a
    floating point `#rotation` value, and canonical values as described in
    the IIIF spec.

--- lib/iiif/image_response.rb
+++ lib/iiif/image_response.rb
@@ -54,7 +54,8 @@ module IIIF
       ##
       # @abstract implement to return the canonical value
       # @return [String] the canonical value fore the request
-      # @see http://iiif.io/api/image/2.0/#canonical-uri-syntax
+      #
+      # @see http://iiif.io/api/image/2.1/#canonical-uri-syntax
       def canonical_value
         raise NotImplementedError
       end
@@ -78,7 +79,8 @@ module IIIF
     #
     # @see http://iiif.io/api/image/2.1/#region
     #
-    # @todo implement square
+    # @todo implement `square`
+    # @todo add better error messages
     class Region < Parameter
       attr_reader :x,:y,:w,:h
       REGEX = /^(?:full)|(?:(?:pct:)?([\d]+,){3}([\d]+))$/
@@ -104,7 +106,7 @@ module IIIF
       #
       # @todo: checking the regex more thoroughly
       def valid?
-        REGEX.match(@requested_value) && ![w,h].include?(0)
+        REGEX =~ @requested_value && ![w,h].include?(0)
       end
 
       ##
@@ -154,6 +156,72 @@ module IIIF
         @full = nil
 
         [x, y, w, h]
+      end
+    end
+
+    ##
+    #
+    class Size < Parameter
+    end
+
+    ##
+    # Represents the rotation parameter of an image request
+    #
+    # Values may be floating points from '0' to '360', with an optional '!' 
+    # indicating mirroring.
+    #
+    # @see http://iiif.io/api/image/2.1/#rotation
+    class Rotation < Parameter
+      REGEX = /^(?<mirror>!)?(?<rotation>\d{1,3}(?:\.\d+)?)$/
+
+      ##
+      # @return [Boolean] `true` if the value is a valid rotation
+      def valid?
+        REGEX =~ @requested_value && in_range?
+      end
+
+      ##
+      # @see Parameter#canonical_value
+      def canonical_value
+        "#{mirror? ? '!' : nil}#{rotation_string}"
+      end
+
+      ##
+      # @return [Boolean] true if the rotation is between 0 and 360
+      def in_range?
+        !rotation.nil? && rotation <= 360 
+      end
+
+      ##
+      # @return [Float, nil] the rotation value as a float. `nil` if none is 
+      #   found
+      def rotation
+        parse.nil? ? nil : parse[:rotation].to_f
+      end
+
+      ##
+      # @return [Boolean] true if the '!' mirror character is present in the
+      #   request value
+      def mirror?
+        parse.nil? ? false : !parse[:mirror].nil?
+      end
+
+      private
+
+      ##
+      # @return [MatchData, nil] a regex match with `:mirror` and `:rotation`
+      #   values. `nil` if no match.
+      def parse
+        @match ||= REGEX.match(@requested_value)
+      end
+
+      ##
+      # @return [String] rotation value as integer if x.0, as float if there 
+      #   is a value right ofthe decimal
+      def rotation_string
+        int_rotation = rotation.to_i
+
+        rotation == int_rotation ? int_rotation.to_s : rotation.to_s
       end
     end
   end
--- spec/lib/iiif/image_response_spec.rb
+++ spec/lib/iiif/image_response_spec.rb
@@ -151,3 +151,78 @@ describe IIIF::ImageResponse::Region do
     end
   end
 end
+
+describe IIIF::ImageResponse::Rotation do
+  it_behaves_like 'a parameter' do
+    let(:args) { [] }
+    let(:valids) { ['0', '!0', '!180', '!180', '360', '!360', '0.1', '!0.1'] }
+    let(:invalids) { ['361', '!361', '360.1', '!360.1', '-1', '!-1', 'a39'] }
+  end
+
+  subject { described_class.new('90') }
+
+  describe '#canonical_value' do
+    it 'gives int version if no decimal values' do
+      expect(subject.canonical_value).to eq '90'
+    end
+
+    it 'gives int version if decimal values is .0' do
+      expect(described_class.new('90.0').canonical_value).to eq '90'
+    end
+
+    it 'gives float version if decimal values are present' do
+      expect(described_class.new('90.1').canonical_value).to eq '90.1'
+    end
+
+    it 'gives mirror character if decimal values are present' do
+      expect(described_class.new('!90.1').canonical_value).to eq '!90.1'
+    end
+
+    it 'has a system precision float cap' do
+      value = '90.1111111111111111111111111111'
+      expect(described_class.new(value).canonical_value).to eq value.to_f.to_s
+    end
+  end
+
+  describe '#in_range?' do
+    it 'is in range for values between 0 and 360' do
+      ['0', '0.1', '90', '360'].each do |val|
+        expect(described_class.new(val)).to be_in_range
+      end
+    end
+
+    it 'is out of range for other values' do
+      ['-1', '-0.1', '900', '360.1'].each do |val|
+        expect(described_class.new(val)).not_to be_in_range
+      end
+    end
+  end
+
+  describe '#rotation' do
+    it 'gives nil when no match' do
+      expect(described_class.new('moomin').rotation).to be_nil
+    end
+
+    it 'gives rotation as float with match' do
+      expect(described_class.new('360').rotation).to eql 360.0
+    end
+
+    it 'gives rotation as float with mirrored match' do
+      expect(described_class.new('!360').rotation).to eql 360.0
+    end
+  end
+
+  describe '#mirror?' do
+    it 'gives false when no match' do
+      expect(described_class.new('moomin')).not_to be_mirror
+    end
+
+    it 'gives false when mirror char is not present' do
+      expect(described_class.new('360')).not_to be_mirror
+    end
+
+    it 'gives true when mirror char is present' do
+      expect(described_class.new('!360')).to be_mirror
+    end
+  end
+end
